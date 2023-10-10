d008e2557f448c81820edcde2b45a6e6496c718d
zapashcanon <leo@ndrs.fr>
2023-10-10 09:34:04 +0200

    remove unneeded file

--- lib/libctrl.ml
+++
@@ -1,7 +0,0 @@
-module Amb = Amb
-module Callcc = Callcc
-module Felleisen_C = Felleisen_C
-module Monadic_reflection = Monadic_reflection
-module Prompt = Prompt
-module Shift_reset = Shift_reset
-module Control_prompt = Control_prompt
